e348a8f0c34dc5dec2c58da9e328c75fa37e16a2
Aishat Adenike Akinyemi <aishatadenike01@gmail.com>
2022-04-28 14:38:12 +0000

    fix  display errors

--- src/components/marketplace/Products.js
+++ src/components/marketplace/Products.js
@@ -73,7 +73,7 @@ const Products = () => {
             <h1 className="fs-4 fw-bold mb-0">Street Food</h1>
             <AddProduct save={addProduct} />
           </div>
-          {/* <Row xs={1} sm={2} lg={3} className="g-3  mb-5 g-xl-4 g-xxl-5">
+          <Row xs={1} sm={2} lg={3} className="g-3  mb-5 g-xl-4 g-xxl-5">
             {products.map((_product) => (
               <Product
                 product={{
@@ -82,7 +82,7 @@ const Products = () => {
                 buy={buy}
               />
             ))}
-          </Row> */}
+          </Row>
         </>
       ) : (
         <Loader />
--- src/utils/marketplace.js
+++ src/utils/marketplace.js
@@ -6,25 +6,24 @@ const GAS = 100000000000000;
 
 
 
-export function createProduct(product) {
+export async function createProduct(product) {
   product.id = uuid4();
   product.price = parseNearAmount(product.price + "");
-  return window.contract.setProduct({ product });
+  return await window.contract.setProduct({ product });
 }
 
 export async function getProducts() {
   try{
     const products = await window.contract.getProducts();
-    console.log(products);
     const productList = [];    
     products.forEach(prod => {
       const prodItem = new Promise( async (resolve) => {
         const meta = await fetchProdMeta(prod.metadata);
         resolve({
-          id: prodItem.id,
-          price: prodItem.price,
-          owner: prodItem.owner,
-          sold: prodItem.sold,
+          id: prod.id,
+          price: prod.price,
+          owner: prod.owner,
+          sold: prod.sold,
           name: meta.data.name,
           image: meta.data.image,
           description: meta.data.description,
